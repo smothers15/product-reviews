c9074ccaa041e56d1dabdcacd986e1a3dc685136
smothers15 <dylo@fresnou.org>
2022-01-20 08:55:35 -0800

    slide 35

---
+++ controllers/productController.js
@@ -0,0 +1,33 @@
+const {Product} = require('../models')
+const categories = ['home goods', 'kitchen', 'appliances', 'other'];
+module.exports.renderProfile = async function(req, res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/profile', {product});
+}
+
+module.exports.renderEditForm = async function(req, res) {
+    const product = await Product.findByPk(
+        req.params.id
+    );
+    res.render('products/edit', {product, categories});
+}
+
+module.exports.updateProduct = async function(req, res) {
+    await Product.update({
+        product_name: req.body.product_name,
+        category: req.body.category,
+        description: req.body.description
+    },{
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/products/profile/${req.params.id}`);
+}
+
+module.exports.viewProducts = async function(req, res) {
+    const products = await Product.findAll();
+    res.render('index', {products})
+}
